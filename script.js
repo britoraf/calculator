c05b87da3ab91e399ea6a371c965976c7c4ed05f
britoraf <rafaelbrito__@outlook.com>
2026-01-11 20:45:12 +0000

    Housing assembly: Aspect ratio and border radius based on real one.

---
+++ script.js
@@ -0,0 +1,35 @@
+// container selection and formating //
+// const container = document.querySelector("#container");
+// container.style.backgroundColor = "yellow";
+// container.style.display = "flex";
+// container.style.flexDirection = "column";
+// container.style.justifyContent = "flex-start";
+// container.style.alignItems = "center";
+// container.style.height = "230px";
+
+// // main elements selection and formating //
+// const calcHeader = document.querySelector("#calcHeader");
+// calcHeader.style.backgroundColor = "red";
+// calcHeader.style.display = "flex";
+// calcHeader.style.flexDirection = "row";
+// calcHeader.style.height = "30px";
+// calcHeader.style.width = "600px";
+// calcHeader.style.margin = "5px";
+
+// const visor = document.querySelector("#visor");
+// visor.style.backgroundColor = "gray";
+// visor.style.display = "flex";
+// visor.style.flexDirection = "row";
+// visor.style.height = "30px";
+// visor.style.width = "600px";
+// visor.style.margin = "5px";
+
+// const keyboard = document.querySelector("#keyboard");
+// keyboard.style.backgroundColor = "purple";
+// keyboard.style.display = "flex";
+// keyboard.style.flexDirection = "row";
+// keyboard.style.height = "30px";
+// keyboard.style.width = "600px";
+// keyboard.style.margin = "5px";
+
+// functions //
